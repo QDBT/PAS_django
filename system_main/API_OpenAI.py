e7ce2005dc745ecd11eb579812cecf7cc5e50108
QDBT <1cda2122@cc.u-tokai.ac.jp>
2024-10-10 17:07:19 +0900

    Set up the sidebar and divider next to this

--- system_main/API_OpenAI.py
+++ system_main/API_OpenAI.py
@@ -18,8 +18,8 @@ def OpenAI_API(contextx,system_message=None):
     combined_content = "".join(contextx)
     messages.append({"role": "user", "content": combined_content})
 
-    print("Messages to be sent to the API:")
-    print(f"{messages}")
+    #print("Messages to be sent to the API:")
+    #print(f"{messages}")
 
     for message in messages:
         if message['role'] == "system":
