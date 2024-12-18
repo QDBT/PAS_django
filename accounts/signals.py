d765ec7bf73d581920050baa791e0bef37f5eb56
QDBT <1cda2122@cc.u-tokai.ac.jp>
2024-12-18 20:54:08 +0900

    code

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,6 +7,40 @@ from homepage.models import Project
 @receiver(post_save, sender=User)
 def create_initial_file(sender, instance, created, **kwargs):
     if created:
+        # Training code for the initial file
+        TranningCode = """
+import random
+
+def guess_the_number_simulated():
+    print("Welcome to 'Guess the Number'!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("Can you guess it?")
+
+    # Generate a random number between 1 and 100
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+    guessed_correctly = False
+
+    # Predefined list of guesses
+    simulated_guesses = [10, 20, 50, 75, 60, 65, 63, number_to_guess]  # Include the correct number eventually
+
+    for guess in simulated_guesses:
+        attempts += 1
+        print(f"Attempt {attempts}: Guessing {guess}...")
+
+        # Check the guess
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print("Too high!")
+        else:
+            print(f"Congratulations! The number was {number_to_guess}. You guessed it in {attempts} attempts.")
+            guessed_correctly = True
+            break
+
+    if not guessed_correctly:
+        print("Oh no! The guesses ran out before finding the number.")
+"""
         # Create a Project instance for the user if necessary
         project, project_created = Project.objects.get_or_create(
             user=instance,
@@ -16,7 +50,7 @@ def create_initial_file(sender, instance, created, **kwargs):
         File.objects.create(
             project=project,
             file_name='Hello.py',
-            code='# Default Python code here'
+            code=TranningCode
         )
     
     
